7f185bb0f8c9c3812b224440fed64c83dd39a199
Doug Beney <contact@dougie.io>
2018-11-05 17:12:44 -0500

    editor fixes

--- src/escriba-textedit.cpp
+++ src/escriba-textedit.cpp
@@ -91,12 +91,18 @@ void Escriba_TextEdit::setCurlineformat(const QTextCharFormat &curlineformat)
 void Escriba_TextEdit::setHtml(const QString html)
 {
 	QString sanitized_html = html;
-
+	sanitized_html.prepend("<style>pre { background: #474747; margin: 20px 10px; font-family: monospace; color: white;}</style>");
 	// Qt's HTML standard does not support <del>.
 	// Instead they support <s>. Do a replace!
 	sanitized_html.replace( "<del>", "<s>" );
 	sanitized_html.replace( "</del>", "</s>" );
 
+	sanitized_html.replace(QRegExp("[\\s\n\\n]+</code>"), "</code>");
+
+	// Add a <p> if a </pre> is the last element in document.
+	// This allows the user to not...be stuck forever in a code block.
+	sanitized_html.replace(QRegExp("</pre>[\n]?$"), "</pre><p></p>");
+	qDebug() << sanitized_html;
 	QTextEdit::setHtml( sanitized_html );
 }
 
